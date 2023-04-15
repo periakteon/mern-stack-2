88643e349d132443e4370877b45a6fda6996341d
periakteon <masumgokyuz@gmail.com>
2023-04-15 15:31:55 +0300

    user router added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
 import connectDB from "./config/dbConnection.js";
+import userRouter from "./routes/user.js";
 dotenv.config();
 
 const app = express();
@@ -24,6 +25,8 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/user", userRouter); // http://localhost:5000/user/signup
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
